4e2cebe47bee872a6cb8e5d4c73f4c1a4d786001
Bryan Long <bryancharleslong@gmail.com>
2019-11-05 10:21:45 -0500

    cloud changes

--- pet-shelter-frontend/js/app.js
+++ pet-shelter-frontend/js/app.js
@@ -5,6 +5,7 @@ import Cages from './components/Cages'
 import Pets from './components/Pets'
 import apiActions from './api/api-actions';
 import '../css/styles.css';
+var apiPath = "http://petshelterapi-env.3q9aiabym9.us-east-2.elasticbeanstalk.com";
 main()
 
 function main() {
@@ -33,20 +34,20 @@ function navCages() {
     const cagesButton = document.querySelector('.nav__cages');
     //get request
     cagesButton.addEventListener('click', function () {
-        apiActions.getRequest('http://localhost:8080/cages', cages => {
+        apiActions.getRequest(apiPath + '/cages', cages => {
             getAppContext().innerHTML = Cages(cages);
         })
     })
     //post request
     getAppContext().addEventListener('click', function () {
         if (event.target.classList.contains('add-cage__submit')) {
-            apiActions.postRequest('http://localhost:8080/cages/add-cage', {
+            apiActions.postRequest(apiPath + '/cages/add-cage', {
             }, (cages) => getAppContext().innerHTML = Cages(cages))
         }
     })
     getAppContext().addEventListener('click', function () {
         if (event.target.classList.contains('remove-cage__submit')) {
-            apiActions.postRequest('http://localhost:8080/cages/remove-cage', {
+            apiActions.postRequest(apiPath + '/cages/remove-cage', {
             }, (cages) => getAppContext().innerHTML = Cages(cages))
         }
     })
@@ -55,8 +56,8 @@ function navCages() {
 function navPets() {
     const petsButton = document.querySelector('.nav__pets');
     petsButton.addEventListener('click', function () {
-        apiActions.getRequest('http://localhost:8080/cages', cages => {
-            apiActions.getRequest('http://localhost:8080/pets', pets => {
+        apiActions.getRequest(apiPath + '/cages', cages => {
+            apiActions.getRequest(apiPath + '/pets', pets => {
                 getAppContext().innerHTML = Pets(pets, cages);
                 //after app content is loaded, start hover listener
                 getAppContext().querySelector("ul").addEventListener('mouseleave', function () {
@@ -72,8 +73,8 @@ function navPets() {
         if (event.target.classList.contains('add-pet__submit')) {
             const petName = event.target.parentElement.querySelector('.add-pet__petName').value;
             const cageId = event.target.parentElement.querySelector('.add-pet__cageName').value;
-            apiActions.getRequest('http://localhost:8080/cages', cages => {
-                apiActions.postRequest('http://localhost:8080/pets/add-pet', {
+            apiActions.getRequest(apiPath + '/cages', cages => {
+                apiActions.postRequest(apiPath + '/pets/add-pet', {
                     petName: petName,
                     cageId: cageId
                 }, (pets) => getAppContext().innerHTML = Pets(pets, cages))
@@ -94,8 +95,8 @@ function navPets() {
             event.target.parentElement.appendChild(deleteButton);
             deleteButton.addEventListener('click', function () {
                 var petId = event.target.parentElement.getAttribute('petId');
-                apiActions.getRequest('http://localhost:8080/cages', cages => {
-                    apiActions.postRequest('http://localhost:8080/pets/remove-pet', {
+                apiActions.getRequest(apiPath + '/cages', cages => {
+                    apiActions.postRequest(apiPath + '/pets/remove-pet', {
                        petId: petId
                     }, (pets) => getAppContext().innerHTML = Pets(pets, cages))
                 })
